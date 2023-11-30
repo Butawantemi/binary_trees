eda1e02621c1bd18205146cb3a08243a51f777a0
Butawantemi <japhetbuta@gmail.com>
2023-11-30 20:19:20 +0300

    binary_trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,10 +8,10 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
-	if (!tree->right && !tree->left)
+	if (tree->right == NULL && tree->left == NULL)
 		return (1);
 
 	if (tree->right && tree->left)
