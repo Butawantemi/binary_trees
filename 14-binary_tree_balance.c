daeb128db1fd92091f69bbb47ac4150aed75a03d
Butawantemi <japhetbuta@gmail.com>
2023-11-30 20:05:06 +0300

    binary_trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,19 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: A pointer to the root node of the tree to measure the balance factor
- * Return: Balance factor of the tree, or 0 if tree is NULL
- */
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-}
-
-/**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: A pointer to the root node of the tree to measure the height
  * Return: Height of the tree, or 0 if tree is NULL
@@ -37,4 +24,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (heighLeft);
 	else
 		return (heightRight);
+}
+
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the balance factor
+ * Return: Balance factor of the tree, or 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree) 
+{
+    if (tree == NULL) {
+        return 0;
+    }
+    size_t left_height = binary_tree_height(tree->left);
+    size_t right_height = binary_tree_height(tree->right);
+    return (int)(left_height - right_height);
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,28 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - checks if a binary tree is full
- * @tree: pointer to the root node
- *
- * full means a node has either zero or two children
- *
- * Return: 0 if tree is NULL
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: A pointer to the root node of the tree to check
+ * Return: 1 if the tree is full, 0 otherwise
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
-	/* node has no children */
-	if (tree->left == NULL && tree->right == NULL)
+	if (!tree->left && !tree->right)
 		return (1);
-
-	/* if node has two children continue checking recursively */
-	if (tree->left != NULL && tree->right != NULL)
+	if (!tree->left && !tree->right)
 		return (binary_tree_is_full(tree->left) &&
 				binary_tree_is_full(tree->right));
-
-	/* if node has only one child it's not full */
 	return (0);
 }
