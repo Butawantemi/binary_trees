71f31a69e85d8278276ad1e5365d8795a203cb4b
Butawantemi <japhetbuta@gmail.com>
2023-11-30 16:39:32 +0300

    binary_trees

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,17 +6,20 @@
  * Return: A pointer to the uncle node, or NULL if node is NULL or has no uncle
  */
 
-binary_tree_t *binary_tree_uncle(binary_tree_t *node) {
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL) {
-        return NULL;  
-    }
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
 
-    binary_tree_t *grand_parent = node->parent->parent;
-    
-   
-    if (grand_parent->left == node->parent) {
-        return grand_parent->right;  
-    } else {
-        return grand_parent->left;
-    }
+	return (Sib(node->parent));
+}
+
+binary_tree_t *Sib(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
